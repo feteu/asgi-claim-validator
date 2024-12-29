c904eb9590490ee370d3c005404c3ba4db79cee3
feteu <29181278+feteu@users.noreply.github.com>
2024-12-29 21:50:45 +0100

    Enhance connexion_simple example by updating HTTP method for UI endpoint and adding error handlers for claim validation exceptions

--- examples/connexion_simple/connexion_simple.py
+++ examples/connexion_simple/connexion_simple.py
@@ -1,8 +1,11 @@
+import json
 import time
 import uvicorn
 from connexion import AsyncApp, RestyResolver
+from connexion.exceptions import OAuthProblem
+from connexion.lifecycle import ConnexionRequest, ConnexionResponse
 from connexion.middleware import MiddlewarePosition
-from asgi_claim_validator import ClaimValidatorMiddleware
+from asgi_claim_validator import ClaimValidatorMiddleware, MissingEssentialClaimException, UnspecifiedPathAuthenticationException
 
 JWT_LIFETIME_SECONDS = 3600
 
@@ -21,7 +24,7 @@ claim_validation_config = {
     "skipped": {
         "^/api/1/openapi.json$": ["get"],
         "^/api/1/skipped/?$": ["get"],
-        "^/api/1/ui/?$": ["post"],
+        "^/api/1/ui/?$": ["get"],
         "^/api/1/ui/.+$": ["get"],
     },
     "secured": {
@@ -43,9 +46,18 @@ claim_validation_config = {
     "claims": claims_mock,
 }
 
+def claim_validator_401(request: ConnexionRequest, exc: Exception) -> None:
+    raise OAuthProblem()
+
+def claim_validator_403(request: ConnexionRequest, exc: Exception) -> ConnexionResponse:
+    return ConnexionResponse(status_code=403, body=json.dumps({"error": "Forbidden"}), content_type="application/json")
+
 app = AsyncApp(__name__, specification_dir="spec")
 app.add_api("openapi.yaml", resolver=RestyResolver("api"))
 app.add_middleware(ClaimValidatorMiddleware, MiddlewarePosition.BEFORE_SWAGGER, **claim_validation_config)
+app.add_error_handler(MissingEssentialClaimException, claim_validator_401)
+app.add_error_handler(UnspecifiedPathAuthenticationException, claim_validator_403)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host='127.0.0.1', port=8000)

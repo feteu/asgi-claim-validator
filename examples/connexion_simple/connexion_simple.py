f54eff5284b9220ce12ee40bd8527d79f2e188a9
feteu <29181278+feteu@users.noreply.github.com>
2025-01-04 20:35:01 +0100

    Refactor connexion_simple example to enhance claim validation configuration and improve error handling for OAuth exceptions

--- examples/connexion_simple/connexion_simple.py
+++ examples/connexion_simple/connexion_simple.py
@@ -2,16 +2,37 @@ import json
 import time
 import uvicorn
 from connexion import AsyncApp, RestyResolver
-from connexion.exceptions import OAuthProblem
+from connexion.exceptions import OAuthProblem, problem
 from connexion.lifecycle import ConnexionRequest, ConnexionResponse
 from connexion.middleware import MiddlewarePosition
-from asgi_claim_validator import ClaimValidatorMiddleware, MissingEssentialClaimException, UnspecifiedPathAuthenticationException
+from asgi_claim_validator import ClaimValidatorMiddleware
+from asgi_claim_validator.exceptions import MissingEssentialClaimException, UnspecifiedPathAuthenticationException
 
 JWT_LIFETIME_SECONDS = 3600
 
-# Mock claims for testing purposes, ideally this should be fetched from the JWT token.
-# usually with connexion the claims are accessible via the state object of connexion.
-claims_mock = lambda: {
+claim_validation_skipped = {
+    "^/api/1/openapi.json$": ["get"],
+    "^/api/1/skipped/?$": ["get"],
+    "^/api/1/ui/?$": ["get"],
+    "^/api/1/ui/.+$": ["get"],
+}
+claim_validation_secured = {
+    "^/api/1/secured$": {
+        "get": {
+            "sub" : {
+                "essential": True,
+                "allow_blank": False,
+                "values": ["admin"],
+            }, 
+            "iss": {
+                "essential": True,
+                "allow_blank": False,
+                "values": ["https://example.com"],
+            },
+        },
+    }
+}
+claim_validation_claims_callable = lambda: {
     "sub": "admin",
     "iss": "https://example.com",
     "aud": "https://example.com",
@@ -20,41 +41,19 @@ claims_mock = lambda: {
     "nbf": int(time.time()),
 }
 
-claim_validation_config = {
-    "skipped": {
-        "^/api/1/openapi.json$": ["get"],
-        "^/api/1/skipped/?$": ["get"],
-        "^/api/1/ui/?$": ["get"],
-        "^/api/1/ui/.+$": ["get"],
-    },
-    "secured": {
-        "^/api/1/secured$": {
-            "get": {
-                "sub" : {
-                    "essential": True,
-                    "allow_blank": False,
-                    "values": ["admin"],
-                }, 
-                "iss": {
-                    "essential": True,
-                    "allow_blank": False,
-                    "values": ["https://example.com"],
-                },
-            },
-        }
-    },
-    "claims": claims_mock,
-}
-
-def claim_validator_401(request: ConnexionRequest, exc: Exception) -> None:
-    raise OAuthProblem()
+def claim_validator_401(request: ConnexionRequest, exc: Exception) -> ConnexionResponse:
+    return problem(
+        detail=exc.detail,
+        status=exc.status,
+        title=exc.title,
+    )
 
 def claim_validator_403(request: ConnexionRequest, exc: Exception) -> ConnexionResponse:
     return ConnexionResponse(status_code=403, body=json.dumps({"error": "Forbidden"}), content_type="application/json")
 
 app = AsyncApp(__name__, specification_dir="spec")
 app.add_api("openapi.yaml", resolver=RestyResolver("api"))
-app.add_middleware(ClaimValidatorMiddleware, MiddlewarePosition.BEFORE_SWAGGER, **claim_validation_config)
+app.add_middleware(ClaimValidatorMiddleware, MiddlewarePosition.BEFORE_SWAGGER, secured=claim_validation_secured, skipped=claim_validation_skipped, claims_callable=claim_validation_claims_callable)
 app.add_error_handler(MissingEssentialClaimException, claim_validator_401)
 app.add_error_handler(UnspecifiedPathAuthenticationException, claim_validator_403)
 

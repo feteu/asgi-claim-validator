a13cfa5c17a77337588c0725e0459c3619112050
feteu <29181278+feteu@users.noreply.github.com>
2025-01-05 18:31:44 +0100

    Add JSON schema definitions for secured and skipped configurations

--- asgi_claim_validator/constants.py
+++ asgi_claim_validator/constants.py
@@ -11,3 +11,81 @@ _DEFAULT_RAISE_ON_UNSPECIFIED_PATH: bool = True
 _DEFAULT_RE_IGNORECASE: bool = False
 _DEFAULT_SECURED: SecuredType = dict()
 _DEFAULT_SKIPPED: SkippedType = dict()
+_DEFAULT_SKIPPED_JSON_SCHEMA: dict = {
+    "title": "Skipped JSON Schema",
+    "description": "Schema for validating skipped configuration",
+    "type": "object",
+    "minProperties": 1,
+    "patternProperties": {
+        "^(.+)$": {
+            "type": "array",
+            "items": {
+                "oneOf": [
+                    {
+                        "type": "string",
+                        "pattern": "^(?i:DELETE|GET|HEAD|OPTIONS|PATCH|POST|PUT|{_DEFAULT_ANY_HTTP_METHOD})$"
+                    },
+                    { "type": "null" }
+                ]
+            }
+        }
+    },
+    "additionalProperties": False,
+    "unevaluatedProperties": False
+}
+_DEFAULT_SECURED_JSON_SCHEMA: dict = {
+    "title": "Secured JSON Schema",
+    "description": "Schema for validating secured configuration",
+    "type": "object",
+    "minProperties": 1,
+    "patternProperties": {
+        "^(.+)$": {
+            "type": "object",
+            "minProperties": 1,
+            "patternProperties": {
+                f"^(?i:DELETE|GET|HEAD|OPTIONS|PATCH|POST|PUT|{_DEFAULT_ANY_HTTP_METHOD})$": {
+                    "type": "object",
+                    "minProperties": 1,
+                    "patternProperties": {
+                        "^(.+)$": {
+                            "type": "object",
+                            "minProperties": 1,
+                            "properties": {
+                                "essential": {
+                                    "oneOf": [
+                                        {"type": "boolean"}
+                                    ]
+                                },
+                                "allow_blank": {
+                                    "oneOf": [
+                                        {"type": "boolean"},
+                                        {"type": "null"}
+                                    ]
+                                },
+                                "values": {
+                                    "type": "array",
+                                    "items": {
+                                        "oneOf": [
+                                            {"type": "boolean"},
+                                            {"type": "integer"},
+                                            {"type": "string"}
+                                        ]
+                                    }
+                                }
+                            },
+                            "required": ["essential", "values"],
+                            "additionalProperties": False,
+                            "unevaluatedProperties": False
+                        }
+                    },
+                    "additionalProperties": False,
+                    "unevaluatedProperties": False
+                }
+            },
+            "additionalProperties": False,
+            "unevaluatedProperties": False
+        }
+    },
+    "additionalProperties": False,
+    "unevaluatedProperties": False
+}

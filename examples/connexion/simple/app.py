e68d31eafe4303a391e4100266584bfc889c1689
feteu <29181278+feteu@users.noreply.github.com>
2025-01-05 20:46:08 +0100

    Refactor claim validator error handling in app.py

--- examples/connexion/simple/app.py
+++ examples/connexion/simple/app.py
@@ -6,7 +6,7 @@ from connexion.exceptions import problem
 from connexion.lifecycle import ConnexionRequest, ConnexionResponse
 from connexion.middleware import MiddlewarePosition
 from asgi_claim_validator import ClaimValidatorMiddleware
-from asgi_claim_validator.exceptions import MissingEssentialClaimException, UnspecifiedPathAuthenticationException
+from asgi_claim_validator.exceptions import ClaimValidatorException, UnspecifiedPathAuthenticationException
 
 JWT_LIFETIME_SECONDS = 3600
 
@@ -41,21 +41,18 @@ claim_validation_claims_callable = lambda: {
     "nbf": int(time.time()),
 }
 
-def claim_validator_401(request: ConnexionRequest, exc: Exception) -> ConnexionResponse:
-    return problem(
-        detail=exc.detail,
-        status=exc.status,
-        title=exc.title,
-    )
 
-def claim_validator_403(request: ConnexionRequest, exc: Exception) -> ConnexionResponse:
+def claim_validator_custom_403(request: ConnexionRequest, exc: Exception) -> ConnexionResponse:
     return ConnexionResponse(status_code=403, body=json.dumps({"error": "Forbidden"}), content_type="application/json")
+
+def claim_validator_error_handler(request: ConnexionRequest, exc: Exception) -> ConnexionResponse:
+    return problem(detail=exc.detail, status=exc.status, title=exc.title)
 
 app = AsyncApp(__name__, specification_dir="spec")
 app.add_api("openapi.yaml", resolver=RestyResolver("api"))
 app.add_middleware(ClaimValidatorMiddleware, MiddlewarePosition.BEFORE_SWAGGER, secured=claim_validation_secured, skipped=claim_validation_skipped, claims_callable=claim_validation_claims_callable)
-app.add_error_handler(MissingEssentialClaimException, claim_validator_401)
-app.add_error_handler(UnspecifiedPathAuthenticationException, claim_validator_403)
+app.add_error_handler(UnspecifiedPathAuthenticationException, claim_validator_custom_403)
+app.add_error_handler(ClaimValidatorException, claim_validator_error_handler)
 
 
 if __name__ == "__main__":

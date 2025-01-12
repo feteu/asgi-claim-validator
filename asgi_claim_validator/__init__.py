8598969905489339fd47fb926de5904c34eb4a4c
feteu <29181278+feteu@users.noreply.github.com>
2025-01-12 17:27:02 +0100

    Refactor imports in __init__.py to enhance clarity and organization of exception and type definitions

--- asgi_claim_validator/__init__.py
+++ asgi_claim_validator/__init__.py
@@ -1,21 +1,35 @@
-from asgi_claim_validator.decorators import validate_claims_callable
 from asgi_claim_validator.exceptions import (
     ClaimValidatorException,
+    InvalidClaimsConfigurationException,
     InvalidClaimsTypeException,
     InvalidClaimValueException,
+    InvalidSecuredConfigurationException,
+    InvalidSkippedConfigurationException,
     MissingEssentialClaimException,
     UnauthenticatedRequestException,
     UnspecifiedMethodAuthenticationException,
     UnspecifiedPathAuthenticationException,
 )
 from asgi_claim_validator.middleware import ClaimValidatorMiddleware
-from asgi_claim_validator.types import SecuredCompiledType, SecuredType, SkippedCompiledType, SkippedType
+from asgi_claim_validator.types import (
+    ClaimsCallableType,
+    ClaimsType,
+    SecuredCompiledType,
+    SecuredType,
+    SkippedCompiledType,
+    SkippedType,
+)
 
 __all__ = (
+    "ClaimsCallableType",
+    "ClaimsType",
     "ClaimValidatorException",
     "ClaimValidatorMiddleware",
+    "InvalidClaimsConfigurationException",
     "InvalidClaimsTypeException",
     "InvalidClaimValueException",
+    "InvalidSecuredConfigurationException",
+    "InvalidSkippedConfigurationException",
     "MissingEssentialClaimException",
     "SecuredCompiledType",
     "SecuredType",
@@ -24,5 +38,4 @@ __all__ = (
     "UnauthenticatedRequestException",
     "UnspecifiedMethodAuthenticationException",
     "UnspecifiedPathAuthenticationException",
-    "validate_claims_callable",
 )
